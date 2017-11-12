299513293788243ae9eab9b73746e8c58de84369
RaulSilveira <raul95@gmail.com>
2017-11-12 14:18:29 -0300

    Read instances and Corss Over

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 int n;
 int initialPopulation;
+int bestCost;
+vector<int> bestSolution;
 vector< vector<int> > graph;
 vector< vector<int> > valids;
 vector< vector<int> > nonValids;
@@ -104,6 +106,7 @@ void randomizeChoromossome(vector<int> &chromossome){
 
 void generatePopulation(){
 	vector<int> chromossome;
+	int flag = true;
 
 	for (int i = 1; i <= n; i++){
 		chromossome.push_back(i);
@@ -111,17 +114,90 @@ void generatePopulation(){
 
 	if(isValid(chromossome)){
 		valids.push_back(chromossome);
+		flag = false;
+		bestSolution = chromossome;
+		bestCost = getCost(chromossome);
 	}else{
 		nonValids.push_back(chromossome);
 	}
 
-	for(int i = 0; i < initialPopulation; i++){
+	int i = 0;
+	while(i < initialPopulation){
 		randomizeChoromossome(chromossome);
 		
 		if(isValid(chromossome)){
 			valids.push_back(chromossome);
+			i++;
+
+			if(flag){
+				flag = false;
+				bestSolution = chromossome;
+				bestCost = getCost(chromossome);
+			}
 		}else{
 			nonValids.push_back(chromossome);
+		}
+	}
+}
+
+void getBestSolution(){
+	for (int i = 1; i < valids.size(); i++){
+		int aux = getCost(valids[i]);
+		if(aux < bestCost){
+			bestCost = aux;
+			bestSolution = valids[i];
+		}
+	}
+
+	printChormossome(bestSolution);
+	printf("Cost: %i\n", bestCost);
+}
+
+void mutation(vector<int> &chromossome){
+	int gene1 = rand()%(n-1) + 1;
+	int gene2 = rand()%(n-1) + 1;
+
+	int aux = chromossome[gene1];
+	chromossome[gene1] = chromossome[gene2];
+	chromossome[gene2] = aux;
+}
+
+vector<int> crossOver(int y, int x){
+	vector<int> father = valids[y];
+	vector<int> mother = valids[x];
+	vector<int> soon;
+
+	for(int i = 0; i < n; i++){
+		if (i % 2 == 0){
+			soon.push_back(father[i]);
+		}else{
+			soon.push_back(mother[i]);
+		}
+	}
+
+	if(!isValid(soon)){
+		mutation(soon);
+	}
+
+	return soon;
+}
+
+void procreation(){
+	for (int i = 0; i < initialPopulation; i++){
+		int father = rand()%(initialPopulation);
+		int mother = rand()%(initialPopulation);
+
+		if(father == mother){
+			i -= 1;
+			continue;
+		}
+
+		vector<int> soon = crossOver(father, mother);
+
+		if(isValid(soon)){
+			valids[father] = soon;
+		}else{
+			nonValids.push_back(soon);
 		}
 	}
 }
@@ -134,6 +210,11 @@ int main(int argc, char* argv[]){
 	srand(time(NULL));
 
 	generatePopulation();
+	getBestSolution();
+
+	procreation();
+
+	getBestSolution();
 
 	return 0;
 }
