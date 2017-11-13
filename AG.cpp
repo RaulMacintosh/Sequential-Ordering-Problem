6dbfeb567d10f8a2e8ef3787a705dc67018cacf3
RaulSilveira <raul95@gmail.com>
2017-11-13 20:41:20 -0300

    Gilney's version

--- AG.cpp
+++ AG.cpp
@@ -1,8 +1,12 @@
+// Authors: Gilney Jr. and Raul Silveira
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
 #include <fstream>
+#include <iostream>
+#include <chrono>
 
 using namespace std;
 
@@ -14,25 +18,25 @@ vector< vector<int> > graph;
 vector< vector<int> > valids;
 vector< vector<int> > nonValids;
 
+// Função que lê o grafo do arquivo
 void readInstance(char* fileName){
 	ifstream fileData;
 	fileData.open(fileName);
 	if(fileData.is_open()){
-		while(!fileData.eof()){
-			fileData >> n;
-			
-			for(int i = 0; i < n; i++){
-				vector<int> line;
-				for(int j = 0; j < n; j++){
-					int x;
-					fileData >> x;
-					line.push_back(x);
-				}
-
-				graph.push_back(line);
+		fileData >> n;
+		graph.clear();
+		for(int i = 0; i < n and !fileData.eof(); i++){
+			vector<int> line;
+			for(int j = 0; j < n; j++){
+				int x;
+				fileData >> x;
+				line.push_back(x);
 			}
+
+			graph.push_back(line);
 		}
 	}
+	fileData.close();
 }
 
 void printInstance(){
@@ -41,22 +45,19 @@ void printInstance(){
 	printf("\n");
 
 	printf("Graph:\n");
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
+	for(size_t i = 0; i < graph.size(); i++){
+		for(size_t j = 0; j < graph.size(); j++){
 			printf("%i ", graph[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-void printChormossome(vector<int> chromossome){
-	printf("[ ");
-
-	for (int i = 0; i < n; i++){
-		printf("%i ", chromossome[i]);
-	}
-
-	printf("]\n");
+void printChormossome(const std::vector<int> &chromossome){
+	std::cout << "[ ";
+	for(auto &e : chromossome)
+		std::cout << e << " ";
+	std::cout << "]\n";
 }
 
 bool isValid(vector<int> chromossome){
@@ -79,43 +80,40 @@ bool isValid(vector<int> chromossome){
 
 int getCost(vector<int> chromossome){
 	int cost = 0;
-	
-	for (int i = 0; i < n-1; i++){
+	for(int i = 0; i < n-1; i++)
 		cost += graph[chromossome[i]][chromossome[i+1]];
-	}
-
 	return cost;
 }
 
 void randomizeChoromossome(vector<int> &chromossome){
-	for(int i = 1; i < n-1; i++){
-		int randomIndex = i + rand()%((n-1)-i);
+	for(int i = 0; i < n; i++){
+		int randomIndex = rand()%n;
 		int aux = chromossome[i];
+		chromossome[i] = chromossome[randomIndex];
+		chromossome[randomIndex] = aux;
+	}
+}
 
-		if(graph[chromossome[randomIndex]][aux] == -1){
-			i -= 1;
-		}else{
-			chromossome[i] = chromossome[randomIndex];
-			chromossome[randomIndex] = aux;
+// Calcula a solução de menor custo
+void getBestSolution()
+{
+	for(auto &e : valids)
+	{
+		int currentCost = getCost(e);
+		if(currentCost < bestCost)
+		{
+			bestSolution = e;
+			bestCost = currentCost;
 		}
 	}
 }
 
+
 void generatePopulation(){
 	vector<int> chromossome;
-	int flag = true;
 
-	for (int i = 1; i <= n; i++){
+	for (int i = 0; i < n; i++){
 		chromossome.push_back(i);
-	}
-
-	if(isValid(chromossome)){
-		valids.push_back(chromossome);
-		flag = false;
-		bestSolution = chromossome;
-		bestCost = getCost(chromossome);
-	}else{
-		nonValids.push_back(chromossome);
 	}
 
 	int i = 0;
@@ -125,26 +123,14 @@ void generatePopulation(){
 		if(isValid(chromossome)){
 			valids.push_back(chromossome);
 			i++;
-
-			if(flag){
-				flag = false;
-				bestSolution = chromossome;
-				bestCost = getCost(chromossome);
-			}
-		}else{
+		}
+		else if(nonValids.size() < (size_t) initialPopulation)
 			nonValids.push_back(chromossome);
-		}
 	}
-}
 
-void getBestSolution(){
-	for (int i = 1; i < valids.size(); i++){
-		int aux = getCost(valids[i]);
-		if(aux < bestCost){
-			bestCost = aux;
-			bestSolution = valids[i];
-		}
-	}
+	bestSolution = valids[0];
+	bestCost = getCost(valids[0]);
+	getBestSolution();
 }
 
 void mutation(vector<int> &chromossome){
@@ -160,7 +146,6 @@ vector<int> crossOver(int y, int x){
 	vector<int> father = valids[y];
 	vector<int> mother = valids[x];
 	vector<int> soon;
-	vector<int> soon2;
 
 	for(int i = 0; i < n; i++){
 		if (i % 2 == 0){
@@ -168,18 +153,6 @@ vector<int> crossOver(int y, int x){
 		}else{
 			soon.push_back(mother[i]);
 		}
-	}
-
-	for(int i = 0; i < n; i++){
-		if (i % 2 == 0){
-			soon2.push_back(mother[i]);
-		}else{
-			soon2.push_back(father[i]);
-		}
-	}
-
-	if(getCost(soon) < getCost(soon2)){
-		soon = soon2;
 	}
 
 	if(!isValid(soon)){
@@ -290,16 +263,23 @@ void procreation(){
 }
 
 int main(int argc, char* argv[]){
-	initialPopulation = atoi(argv[1]);
-	readInstance(argv[2]);
+	initialPopulation = atoi(argv[2]);
+	int num_generations;
+	if(argc == 4)
+		num_generations = atoi(argv[3]);
+	else
+		num_generations = 100;	// Default number
 
+	std::cout << "> Reading graph from file...\n";
+	readInstance(argv[1]);
 	srand(time(NULL));
 
+	std::cout << "> Performing genetic algorithm...\n";
+	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+	
 	generatePopulation();
-	getBestSolution();
-
 	for (int i = 0; i < n; i++){ // N perturbações
-		for (int j = 0; j < n; j++){ // N gerações
+		for (int j = 0; j < num_generations; j++){ // Gerações
 			procreation();
 			getBestSolution();
 		}
@@ -311,8 +291,15 @@ int main(int argc, char* argv[]){
 		}
 	}
 
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	std::cout << "> Best solution: \n   "; 
 	printChormossome(bestSolution);
-	printf("Cost: %i\n", bestCost);
+	std::cout << "> Best solution cost: " << bestCost << "\n";
+
+	std::cout << "\t> " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << " s" << std::endl;
+	std::cout << "\t> " << std::chrono::duration <double, std::milli> (end - start).count() << " ms" << std::endl;
+	std::cout << "\t> " << std::chrono::duration <double, std::nano> (end - start).count() << " ns" << std::endl;
 
 	return 0;
 }
